8444dde536e74abcbbb59f4da1020609935f10fb
Jeyson Jaramillo <jjaramillo@viamatica.com>
2025-02-13 21:36:32 -0500

    Inicio de ejercicio para mantenimiento de perfiles

--- src/app/modules/admin/admin/profile/profile.component.ts
+++ src/app/modules/admin/admin/profile/profile.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { UserI } from '../../../../interfaces/user.interface';
+import { lusers } from '../../../../datasource/user.datasource';
+import { ProfileI } from '../../../../interfaces/profile.interface';
+import { lprofiles } from '../../../../datasource/profile.datasource';
+import { ProfileUserI } from '../../../../interfaces/profile-user.interface';
+import { lprofileuser } from '../../../../datasource/profile-user.datasource';
 
 @Component({
   selector: 'app-profile',
@@ -7,4 +13,11 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
+  userid: any;
+  username: string = '';
+  email: string = '';
+
+  users: UserI[] = lusers;
+  profiles: ProfileI[] = lprofiles;
+  profileUser: ProfileUserI[] = lprofileuser;
 }
