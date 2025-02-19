03bf8b2e290390b1202c455e347a501869f7880a
Jeyson Jaramillo <jjaramillo@viamatica.com>
2025-02-18 20:27:40 -0500

    Validaciones con multiples controles y formularios reactivos

--- src/app/modules/cars/maintenance/maintenance.component.ts
+++ src/app/modules/cars/maintenance/maintenance.component.ts
@@ -19,8 +19,9 @@ export class MaintenanceComponent implements OnInit {
   initForm() : FormGroup {
     return this.frmBuilder.group({
       marca: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)] ],
-      modelo: ['', [Validators.required, Validators.minLength(8)] ],
-      anio: [2000, [Validators.pattern('[0-9]*$'), Validators.minLength(4), Validators.max(2025) ]]
+      modelo: ['', [Validators.pattern('[A-Z]*$') ,Validators.required, Validators.minLength(8)] ],
+      anio: [2000, [Validators.pattern('[0-9]*$'), Validators.minLength(4), Validators.max(2025) ]],
+      tipo: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(5)]]
     });
   }
 
