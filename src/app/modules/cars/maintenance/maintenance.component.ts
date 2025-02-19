59c4632238e889c3f767d6c0da8dffab41fd9efa
Jeyson Jaramillo <jjaramillo@viamatica.com>
2025-02-18 19:30:54 -0500

    Ajuste a campo de formulario reactivo

--- src/app/modules/cars/maintenance/maintenance.component.ts
+++ src/app/modules/cars/maintenance/maintenance.component.ts
@@ -18,8 +18,8 @@ export class MaintenanceComponent implements OnInit {
 
   initForm() : FormGroup {
     return this.frmBuilder.group({
-      marca: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(20)]],
-      modelo: ['', [Validators.required, Validators.minLength(8), Validators.nullValidator]],
+      marca: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)] ],
+      modelo: ['', [Validators.required, Validators.minLength(8), Validators.nullValidator] ],
       anio: [2000, [Validators.pattern('[0-9]*$'), Validators.minLength(4), Validators.max(2025) ]]
     });
   }
